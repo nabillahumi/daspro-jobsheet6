0f078c0e54b8fd045d4e22deac94ea39614137ed
nabillahumi <nabillahumi003@gmail.com>
2024-10-03 13:50:48 +0700

    jobsheet 6 pemilihan 2

--- Pemilihan2Percobaan118.java
+++ Pemilihan2Percobaan118.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan118 {
 
     public static void main(String[] args) {
-        Scanner inputAbsen = new Scanner(System.in);
+        Scanner input18 = new Scanner(System.in);
 
         int tahun;
 
         System.out.print("Masukkan tahun :");
-        tahun = inputAbsen.nextInt();
+        tahun = input18.nextInt();
 
-        if ((tahun % 4) == 0) {
+        if ((tahun%4) == 0 ) {
             if ((tahun % 100) != 0)
-               System.out.println("Tahun Kabisat");
-        } else 
-          System.out.println("Bukan Tahun Kabisat");
-
-
+                System.out.println("Tahun Kabisat");
+            else {
+                System.out.println("Bukan Tahun Kabisat");
+            }    
+        } else
+            System.out.println("bukan Tahun Kabisat");
     }
 }
---
+++ Pemilihan2Percobaan218.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan218 {
+
+    public static void main(String[] args) {
+        Scanner inputAbsen = new Scanner(System.in);
+    }
+    
+}
